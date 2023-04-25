6ce18c62d776f2198388345fb620435f024aaac7
Sachin Holla <51310506+sachinholla@users.noreply.github.com>
2023-04-26 04:14:59 +0530

    AppInterface enhancements for subscription (#67)
    * AppInterface enhancements for subscription
    
    Added new translateSubscribe() and processSubscribe() functions to
    appInterface as per HLD https://github.com/sonic-net/SONiC/pull/1287
    
    Also added stub implementation of these functions to all existing app
    modules. It blindly treats all paths as non-db. Basic subscription
    features, without on_change, can be verified with this mapping.
    
    Signed-off-by: Sachin Holla <sachin.holla@broadcom.com>
    
    * Update error types, test case styles, nil checks, etc
    
    * Remove tlerr import alias in yanglib_app
    
    * Non-pointer params in transleSubscribe and processSubscribe
    
    * Reorg old & new version comprision logic in EntryCompare func
    
    * appInterface method cleanup in acl_app
    
    ---------
    
    Signed-off-by: Sachin Holla <sachin.holla@broadcom.com>

---
+++ translib/subscribe_app_utils.go
@@ -0,0 +1,69 @@
+////////////////////////////////////////////////////////////////////////////////
+//                                                                            //
+//  Copyright 2023 Broadcom. The term Broadcom refers to Broadcom Inc. and/or //
+//  its subsidiaries.                                                         //
+//                                                                            //
+//  Licensed under the Apache License, Version 2.0 (the "License");           //
+//  you may not use this file except in compliance with the License.          //
+//  You may obtain a copy of the License at                                   //
+//                                                                            //
+//     http://www.apache.org/licenses/LICENSE-2.0                             //
+//                                                                            //
+//  Unless required by applicable law or agreed to in writing, software       //
+//  distributed under the License is distributed on an "AS IS" BASIS,         //
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  //
+//  See the License for the specific language governing permissions and       //
+//  limitations under the License.                                            //
+//                                                                            //
+////////////////////////////////////////////////////////////////////////////////
+
+package translib
+
+import (
+	"fmt"
+
+	"github.com/Azure/sonic-mgmt-common/translib/db"
+	"github.com/openconfig/ygot/ygot"
+)
+
+// emptySubscribeResponse returns a translateSubResponse containing a non-db mapping
+// for the given path
+func emptySubscribeResponse(reqPath string) (translateSubResponse, error) {
+	p, err := ygot.StringToStructuredPath(reqPath)
+	if err != nil {
+		return translateSubResponse{}, err
+	}
+	appInfo := &notificationAppInfo{
+		path:                p,
+		dbno:                db.MaxDB, // non-DB
+		isOnChangeSupported: false,
+	}
+	return translateSubResponse{
+		ntfAppInfoTrgt: []*notificationAppInfo{appInfo},
+	}, nil
+}
+
+// translateSubscribeBridge calls the new translateSubscribe() on an app and returns the
+// responses as per old signature. Will be removed after enhancing translib.Subscribe() API
+func translateSubscribeBridge(path string, app appInterface, dbs [db.MaxDB]*db.DB) (*notificationOpts, *notificationInfo, error) {
+	var nAppInfo *notificationAppInfo
+	resp, err := app.translateSubscribe(translateSubRequest{path: path, dbs: dbs})
+	if err == nil && len(resp.ntfAppInfoTrgt) != 0 {
+		nAppInfo = resp.ntfAppInfoTrgt[0]
+	}
+	if nAppInfo == nil {
+		return nil, nil, fmt.Errorf("subscribe not supported (%w)", err)
+	}
+
+	nOpts := &notificationOpts{
+		isOnChangeSupported: nAppInfo.isOnChangeSupported,
+		pType:               nAppInfo.pType,
+		mInterval:           nAppInfo.mInterval,
+	}
+	nInfo := &notificationInfo{dbno: nAppInfo.dbno}
+	if !nAppInfo.isNonDB() {
+		nInfo.table, nInfo.key = *nAppInfo.table, *nAppInfo.key
+	}
+
+	return nOpts, nInfo, nil
+}
