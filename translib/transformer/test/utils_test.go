e193054ce14e56d552383c36cbda36bd1f29f325
ranjinidn <ranjini_nagaraj@dell.com>
2023-05-03 16:53:09 -0700

    Corrected copyright info

--- translib/transformer/test/utils_test.go
+++ translib/transformer/test/utils_test.go
@@ -1,7 +1,6 @@
 ////////////////////////////////////////////////////////////////////////////////
 //                                                                            //
-//  Copyright 2019 Broadcom. The term Broadcom refers to Broadcom Inc. and/or //
-//  its subsidiaries.                                                         //
+//  Copyright 2023 Dell, Inc. 						      //
 //                                                                            //
 //  Licensed under the Apache License, Version 2.0 (the "License");           //
 //  you may not use this file except in compliance with the License.          //
